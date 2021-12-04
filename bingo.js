25206243a97123ee3f03d7ceccf86977b7e01fa7
Ivan Yuan <ivan2100736@gmail.com>
2021-12-04 17:38:48 +0800

    finish

--- bingo.js
+++ bingo.js
@@ -1,42 +1,57 @@
-var initRange="1-30";
-var isUsed = new Array(30)
-
 function getRnage(){    
-    let i= "";
-    i = document.getElementById("p-range").value;
-    let tmp = "";
-    if (i.length > 0){        
-        tmp = i.split("-")      
+    let i = document.getElementById("p-range").value;
+    const min = i.split("-")[0]
+    const max = i.split("-")[1];
+
+    if (min && max && max - min >= 8 && min >0){        
+      tmp = [min , max]
+      console.log('a')
     } else {        
-        tmp = initRange.split("-")        
+       tmp = [1,30]
+       alert('請輸入合理範圍')
     }
     return tmp;
 }
 
 function changeMode() {
     let open=document.getElementById("mode");
+    if(open==0){
+        document.querySelector('#bingoCard').onclick=e=> e.target.classList.toggle("bg-danger");
+        // console.log("aa")
+    } else {
+         document.querySelector('#bingoCard').onclick=undefined;
+         document.querySelectorAll('.bingo-box').forEach(e => e.classList.remove("bg-danger"))
+        //  console.log("bb")
+    }
     open = parseInt(open.value)
     if(open){
         var inputs =document.querySelectorAll(".bingo-box")
-        console.log(inputs)}
-        
+        // console.log(inputs)
         for(let input of inputs){
             input.readOnly = !input.readOnly;
-            console.log(input.readOnly)
+            // console.log(input.readOnly)
         }
-    
+    }
 }
-    
+
+var checkNum = function(){
+    let inUsed = document.querySelectorAll(".bingo-box");
+    let inUsedNums = []
+    inUsed.forEach(item => inUsedNums.push(item.value))
+    // console.log(inUsedNums);
+    document.querySelectorAll('#bingoCard').onchange = item => {
+        // if (item.target.value==)
+}    }
 
 
 function getNewNum(min,Max) {
-    return Math.floor((Math.random()*(Max-min)+1)+min);
+    return Math.floor((Math.random()*(Max-min+1))+min);
 }
 
 const newCard = () => {
-    let RR=getRnage();
-    const min = parseInt(RR[0])
-    const Max = parseInt(RR[1])
+    let NR=getRnage();
+    const min = parseInt(NR[0])
+    const Max = parseInt(NR[1])
     let numberUsed =[];
     for (let i = 0; i < 9; i ++) {
         const randomNum = getNewNum(min,Max);
@@ -47,6 +62,13 @@ const newCard = () => {
             document.getElementById("item" + i).value = randomNum;
         }
     }
-    document.querySelector('#bingoCard').onclick=e=> e.target.classList.toggle("bg-danger");
+    let open=document.getElementById("mode").value;
+    // open = parseInt(open)
     
-}
+        document.querySelector('#bingoCard').onclick=e=> e.target.classList.toggle("bg-danger");
+        console.log("aa")
+    
+    // console.log(document.querySelectorAll(".bingo-box"))
+    
+    }
+    
